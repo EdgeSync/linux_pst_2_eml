70ca6cfe8a33a2a1f88f3ac08ce18b217107fdd7
stauntoc <chris.staunton@hpe.com>
2018-10-17 10:59:02 +0100

    Added {} around the path values - this helps to escape any characters in the path which might break the script.

--- md5rename.sh
+++ md5rename.sh
@@ -3,7 +3,7 @@ for i in $(find | grep ".eml")
 do  
     path=`dirname $i`
     md5=`md5sum ${i}| awk '{print $1}'`
-    newfile=`mv ${i} $path"/"$md5".eml"`
+    newfile=`mv ${i} ${path}"/"$md5".eml"`
     count=$((count+1))
 done
 echo "Total Files Renamed: " $count
--- to_eml.sh
+++ to_eml.sh
@@ -2,7 +2,7 @@ count=0
 for i in $(find | grep ".pst")
 do
     echo "Working on "$i
-    readpst -M -b -q -e -o export $i
+    readpst -M -b -q -e -o export ${i}
     count=$((count+1))
 done
 echo "Total PSTs Converted: " $count
